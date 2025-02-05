c5bd40b79b93fbc5bcad5c00128e191f3d24b6fe
Shaikha <shaikha.saalem@outlook.com>
2025-02-05 08:29:15 +0100

    fix: add ami ids as output
    Signed-off-by: Shaikha <shaikha.saalem@outlook.com>

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "MQTT_EC2_ID" {
     description = "ID of the MQTT EC2 instance"
     value = aws_instance.MQTT_BROKER.id
 }
+
+output "AMI_LINUX_ID" {
+    description = "ID of linux ami"
+    value = data.aws_ami.Linux_AMI.id
+}
+
+output "AMI_UBUNTU_ID" {
+    description = "ID of Ubuntu ami"
+    value = data.aws_ami.Ubuntu_AMI.id
+}
